7933ba29b29c681caae1cb88e231fd9f489100db
LeKimLong <longkimle2004@gmail.com>
2025-11-04 10:25:09 +0700

    Delete Backend/Electric Vehicles Dealer Management System directory
    Delete for new commit with no existing feedback and test drive

--- Backend/Electric Vehicles Dealer Management System/src/main/java/org/example/service/CarModelService.java
+++
@@ -1,8 +0,0 @@
-package org.example.service;
-
-import java.util.List;
-
-public interface CarModelService {
-    List<String> getAllModelNames();
-    String getSegmentByModelName(String modelName);
-}
