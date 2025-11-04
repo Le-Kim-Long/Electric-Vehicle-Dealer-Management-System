e52bda0e20ebcd00b98e8a8380491033321af241
LeKimLong <longkimle2004@gmail.com>
2025-11-04 11:14:53 +0700

    Delete Backend/Electric Vehicles Dealer Management System/src/main/java directory

--- Backend/Electric Vehicles Dealer Management System/src/main/java/org.example/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package org.example.service;
-
-import org.example.dto.LoginRequest;
-import org.example.dto.LoginResponse;
-
-public interface AuthService {
-    LoginResponse login(LoginRequest request);
-}
